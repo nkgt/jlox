bb7282975acb8e9a2fb8f5e917386b9a5356637e
Nicolò Ginatta <nicolo.ginatta@gmail.com>
2023-09-20 21:56:10 +0900

    Adds AST interpreter

--- jlox/src/main/java/com/nkgt/jlox/Lox.java
+++ jlox/src/main/java/com/nkgt/jlox/Lox.java
@@ -9,6 +9,11 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
+    private static final Interpreter interpreter = new Interpreter();
+
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -25,6 +30,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     public static void runPrompt() throws IOException {
@@ -51,15 +57,11 @@ public class Lox {
         if(hadError) return;
 
         System.out.println(new ASTPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
-    }
-
-    private static void report(int line, String where, String message) {
-        System.err.println("[line " + line + "] Error" + where + ": " + message);
-        hadError = true;
     }
 
     static void error(Token token, String message) {
@@ -69,5 +71,14 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
-    static boolean hadError = false;
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
 }
