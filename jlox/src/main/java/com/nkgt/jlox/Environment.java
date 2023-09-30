cb1155b31507f69e090c5e7b025992ee53c9dea1
Nicolò Ginatta <nicolo.ginatta@gmail.com>
2023-10-01 01:29:36 +0900

    Fixes variable binding

--- jlox/src/main/java/com/nkgt/jlox/Environment.java
+++ jlox/src/main/java/com/nkgt/jlox/Environment.java
@@ -19,6 +19,16 @@ public class Environment {
         values.put(name, value);
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+
+        for(int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
     void assign(Token name, Object value) {
         if(values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
@@ -33,6 +43,10 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     Object get(Token name) {
         if(values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -44,5 +58,9 @@ public class Environment {
             name,
             "Undefined variable '" + name.lexeme + "'."
         );
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
     }
 }
--- jlox/src/main/java/com/nkgt/jlox/Interpreter.java
+++ jlox/src/main/java/com/nkgt/jlox/Interpreter.java
@@ -1,11 +1,14 @@
 package com.nkgt.jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
     private final ASTPrinter printer = new ASTPrinter();
 
     Interpreter() {
@@ -42,27 +45,6 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         System.out.println(ASTOutput);
     }
 
-    private String stringify(Object object) {
-        if(object == null) return "nil";
-
-        if(object instanceof Double) {
-            String text = object.toString();
-            if(text.endsWith(".0")) {
-                text = text.substring(0, text.length() - 2);
-            }
-
-            return text;
-        }
-
-        return object.toString();
-    }
-
-    @Override
-    public Void visitBlockStmt(Stmt.Block stmt) {
-        executeBlock(stmt.statements, new Environment(environment));
-        return null;
-    }
-
     void executeBlock(List<Stmt> statements, Environment environment) {
         Environment previous = this.environment;
 
@@ -75,6 +57,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         } finally {
             this.environment = previous;
         }
+    }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
     }
 
     @Override
@@ -99,7 +91,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
     }
 
     @Override
@@ -260,7 +252,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = expr.value.accept(this);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if(distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
+
         return value;
     }
 
@@ -284,5 +283,30 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     private void checkNumberOperands(Token operator, Object left, Object right) {
         if(left instanceof Double && right instanceof Double) return;
         throw new RuntimeError(operator, "Operands must be numbers");
+    }
+
+    private String stringify(Object object) {
+        if(object == null) return "nil";
+
+        if(object instanceof Double) {
+            String text = object.toString();
+            if(text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+
+            return text;
+        }
+
+        return object.toString();
+    }
+
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+
+        if(distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
 }
